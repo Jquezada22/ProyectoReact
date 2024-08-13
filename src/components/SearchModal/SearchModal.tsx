1fb1a4ac7e783d85e7868599550e650ee694b6bf
Jquezada22 <jquezada22abc@gmail.com>
2024-08-12 23:35:10 -0500

    SearchFeatures (#5)
    Se añadio la busqueda de pokemons y estilizo los botones

---
+++ src/components/SearchModal/SearchModal.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { useGetPokemon } from "../../hooks/useGetPokemon";
+import Modal from 'react-modal';
+import { PokemonCard } from "../PokemonCard/PokemonCard";
+import { useSearchStore } from "../../store/useSearchStore";
+
+const SearchModal: React.FC = () => {
+    const [isOpen, closeModal] = useSearchStore((state) => [state.isOpen, state.closeModal]);
+    const [filter, setFilter] = useState("");
+    const [currentSearch, setCurrentSearch] = useState("");
+    const {pokemonData} = useGetPokemon(currentSearch);
+
+    useEffect(() => {
+        if (isOpen) {
+            setFilter("");
+            setCurrentSearch("");
+        }
+    }, [isOpen]);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+        setFilter(event.target.value);
+
+    const onClickSearch = () => setCurrentSearch(filter.toLowerCase());
+
+    const handleCloseModal = () => {
+        setFilter("");
+        setCurrentSearch("");
+        closeModal();
+    }
+
+    return (
+        <Modal isOpen={isOpen} onRequestClose={handleCloseModal}
+        className={"w-6/12 h-4/12 bg-white mx-auto p-5 mt-5 flex flex-col gap-5 items-center shadow-lg rounded-lg"}>
+            <h6>Busqueda</h6>
+            <input type="text" value={filter} onChange={handleInputChange} className="border p-2"/>
+            <button onClick={onClickSearch}>Buscar</button>
+            {pokemonData?.id && <PokemonCard pokemonId={pokemonData.id}/>}
+        </Modal>
+    );
+};
+
+export default SearchModal;
