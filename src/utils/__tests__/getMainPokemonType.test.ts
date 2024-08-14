cd5de5522bc07a25892157d57f94e3f6d9865793
Javier Quezada <jquezada22abc@gmail.com>
2024-08-14 01:30:38 -0500

    Pruebas Unitarias
    Se realizaron pruebas unitarias a los componentes y utilidades

---
+++ src/utils/__tests__/getMainPokemonType.test.ts
@@ -0,0 +1,42 @@
+import { PokemonData } from "../../interfaces/PokemonData";
+import { getMainPokemonType } from "../getMainPokemonType";
+
+describe("getMainPokemonType", () => {
+    it("sould return the first type if there are many types", () => {
+        const pokemonData: PokemonData = {
+            types: [
+                {
+                    slot: 1,
+                    type: {
+                        name: "fire",
+                        url: "https://pokeapi.co/api/v2/type/10",
+                    },
+                },
+                {
+                    slot: 2,
+                    type: {
+                        name: "water",
+                        url: "https://pokeapi.co/api/v2/type/11",
+                    },
+                },
+            ],
+            name: "",
+            id: 0,
+            height: 0,
+            weight: 0,
+            sprites: { front_default: "" },
+        };
+        expect(getMainPokemonType(pokemonData)).toBe("fire");
+    });
+    it("sould return undefined if the pokemon has no types", () => {
+        const pokemonData: PokemonData = {
+            types: [],
+            name: "",
+            id: 0,
+            height: 0,
+            weight: 0,
+            sprites: { front_default: "" },
+        };
+        expect(getMainPokemonType(pokemonData)).toBeUndefined();
+    });
+});
